afcb59b6ae007b40e485831132dd0260a4b32e2c
leofreirerrr <leofreirerr@gmail.com>
2025-01-22 14:51:48 -0300

    codigo pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import Modal from "./components/modal.tsx";
+import style from"./App.module.css"
+import Modal from "./components/Modal.tsx";
 function App() {
   const [open, setOpen] = useState<boolean>(false);
   function closeModal() {
@@ -7,20 +8,10 @@ function App() {
   }
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <button onClick={() => setOpen(true)}>Open modal</button>
+      <div className={style.container}>
+        <button className={ style.btnActivate} onClick={() => setOpen(true)}>Open modal</button>
         <Modal open={open} closeModal={closeModal} />
-
-        {/* <section className="bg-slate-50 text-black p-6 text-center rounded-md space-y-4">
-            <p className="text-3xl">
-              você tem certeza que deseja redefinir a senha?
-            </p>
-            <p id="modal-modal-description">
-              Essa ação é irreversivel
-            </p>
-            <button className="bg-green-300" onClick={() => setOpen(false)}>sim</button>
-            
-          </section> */}
+      
       </div>
     </>
   );
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,5 @@
+import style from "./Modal.module.css";
+
 interface ModalProps {
   open: boolean;
   closeModal: () => void;
@@ -5,25 +7,16 @@ interface ModalProps {
 
 function Modal({ open, closeModal }: ModalProps) {
   return (
-    <section
-      className={`bg-blue-50 absolute p-6 shadow-sm m-2 ${
-        open ? "block" : "hidden"
-      } transform-transition duration-700 ease-in-out ${
-        open ? "translate-y-0" : "translate-y-2"
-      }  rounded-md text-black`}
-    >
-      <p className="text-3xl">você tem certeza que deseja redefinir a senha?</p>
-      <p className="text-slate-500">Alerta: Essa ação é irreversivel</p>
-      <section className="flex justify-between">
-        <button
-          onClick={closeModal}
-          className="bg-transparent border-hidden focus:outline-none "
-        >
-          Voltar
-        </button>
-        <button onClick={closeModal} className="bg-slate-500 text-white">
-          Confirmar
-        </button>
+    <section className={open ? style.modalContainer : style.modalHidden}>
+      <p className={style.title}>você tem certeza que deseja redefinir a senha?</p>
+      <p className={style.subtitle}>Alerta: Essa ação é irreversivel</p>
+      <section className={style.btnContainer}>
+          <button onClick={closeModal} className={style.btnDeny}>
+            Voltar
+          </button>
+          <button onClick={closeModal} className={style.btnConfirm}>
+            Confirmar
+          </button>
       </section>
     </section>
   );
