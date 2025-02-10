7faa019020c468346fda911ee1b7d2dbc7f397d0
jP <silva.joao@icen.ufpa.br>
2025-02-10 15:59:35 -0300

    ta quase pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import style from "./App.module.css";
 import Modal from "./components/Modal.tsx";
 import Toast from "./components/Toast.tsx";
 function App() {
   const [open, setOpen] = useState<boolean>(false);
   const [showToaster, setShowToaster] = useState<boolean>(false);
+  const [mensagem, setMensagem] = useState<string>("")
+  const [toastMode, setToastMode] = useState<"success" | "error" | "alert">("alert")
+  const timeoutRef = useRef<number | null>(null);
   function closeModal() {
     setOpen(false);
   }
   function closeToaster() {
     setShowToaster(false);
   }
+  function openToast(modoToast:"success" | "error" | "alert", mensagem:string) {
+    if (timeoutRef.current !== null) {
+          clearTimeout(timeoutRef.current);
+        }
+    
+    if (showToaster){
+      setShowToaster(false)
+    }
+    setShowToaster(true)
+    setMensagem(mensagem)
+    setToastMode(modoToast)
+    setTimeout(closeToaster, 2500);
+    timeoutRef.current = setTimeout(closeToaster, 2500);
+  }
   return (
     <>
       <div className={style.container}>
-        <button
-          className={style.btnActivate}
-          onClick={() => setShowToaster(true)}
-        >
+        <button className={style.btnActivate} onClick={() => openToast("error","aahh xibiuzão grandão")}>
           Open modal
         </button>
         <Modal open={open} closeModal={closeModal} />
-        <Toast
-          mensagem="ola"
-          closeToaster={closeToaster}
-          show={showToaster}
-          toastMode='success'
-        />
+        {showToaster && <Toast mensagem={mensagem} toastMode={toastMode} closeToaster={closeToaster}/>}
       </div>
     </>
   );
--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,34 +1,25 @@
+// import { useEffect, useRef } from "react";
 import style from "./Toast.module.css";
 interface ToastProps {
   mensagem: string;
-  show: boolean;
   closeToaster: () => void;
   toastMode: "success" | "error" | "alert";
 }
 
-function Message({
-  mensagem,
-  show,
-  closeToaster,
-  toastMode,
-}: ToastProps) {
-  if (show) {
-    setTimeout(closeToaster, 2500);
-
-    const mode =
-      toastMode == "error"
-        ? style.errorToast
-        : toastMode == "alert"
-        ? style.alertToast
-        : style.succesToast;
-    return (
-      <section className={style.succesToast}>
-        <p>{mensagem}</p>
-        <button onClick={() => closeToaster()} className={style.toasterButton}>
-          X
-        </button>
-      </section>
-    );
-  }
+function Message({ mensagem, closeToaster, toastMode }: ToastProps) {
+  const mode =
+    toastMode == "error"
+      ? style.errorToast
+      : toastMode == "alert"
+      ? style.alertToast
+      : style.successToast;
+  return (
+    <section className={`${mode} ${style.toasterContainer}`}>
+      <p>{mensagem}</p>
+      <button onClick={() => closeToaster()} className={style.toastButton}>
+        X
+      </button>
+    </section>
+  );
 }
 export default Message;
